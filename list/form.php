98ac89dbdc2313b8b655043fc7fa1a27444d9078
Jason Peak <jpeak5@lsu.edu>
2012-04-07 14:14:22 -0500

    ok, ajax working nicely between new_file and form.php

--- list/form.php
+++ list/form.php
@@ -1,15 +1,21 @@
 <?php
 require_once('config.php');
+require_once(SPYC);
 
 global $logger;
 
-
-if(isset($_POST[''])){
+print_r($_POST);
+if(isset($_POST['form'])){
 	$logger->log(0,"form.php", "\$post is set proceeding to process request");
-	$formInput = FORMS_PATH.DS."formInput.yaml";
-	$form = new Form("index.php", $formInput, "shopping");
+	
+	
+	if($_POST['form']=="shopping"){
+	$form = new Form("index.php", FORMS_PATH.DS."formInput.yaml", "shopping");
+	}elseif($_POST['form']=="todo"){
+		$form = new Form("index.php", FORMS_PATH.DS."todoInputs.yaml", "todo");
+	}
 
-	$page.=$form->toString();
+	$page=$form->toString();
 	echo $page;
 	$logger->log(0,"index.php::buildPage()", "presenting form defined in {$formInput}");
 
