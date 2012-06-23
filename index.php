b9462818e4f5c059759c1ae9bcc5d3a12a83867b
Jason Peak <jpeak5@lsu.edu>
2012-06-23 18:07:42 -0500

    working

--- index.php
+++ index.php
@@ -118,27 +118,7 @@ $shoppingList = Lists::parseGroceryList(Lists::getList("ShoppingList"));
 $list = "<div id=\"grocery_list\">";
 $list.="<h3>Shopping</h3>";
 
-if(!empty($shoppingList)){
-	foreach($shoppingList as $store=>$items){
-		//	echo "\$store";
-		//	krumo($store);
-		$storeTotal = 0;
-		$listHead="";
-		$listHead.="<span class=\"heading\"><strong>".$store;
-		$listBody = "<ul>";
-		foreach($items as $item){
-			$listBody.="<li class=\"litem\"><strong>{$item->item}</strong>&nbsp;(\${$item->price_estimate})";
-			$listBody.="&nbsp;<a class=\"deleteme\" href=\"javascript:confirmDelete('delete.php?ShoppingList=".urlencode($item->id)."')\">delete</a>";
-			$listBody.="&nbsp;<a class=\"editme\" href=\"index.php?ShoppingList=".urlencode($item->id)."\">edit</a>";
-			$storeTotal+=isset($item->price_estimate) ? $item->price_estimate: 0;
-			$listBody.=(strlen($item->description)>0)?"<br/><span class=\"description\"> ".$item->description."</span>":"";
-			$listBody.="</li>";
-		}
-		$listHead.="</strong><span class=\"aggregate\">&nbsp;&nbsp;(\$".$storeTotal.")</span></span>";
-		$listBody.="</ul>";
-		$list.=$listHead.$listBody;
-	}
-}
+$list.= ListView::RenderShoppingList($shoppingList);
 $list.="</div>";
 
 $content.=$list."</div>";
@@ -150,28 +130,7 @@ $todoList = Lists::parseTodoList(Lists::getList("TodoList"));
 $list= "<div id=\"todo_list\">";
 $list.="<h3>TODOs</h3>";
 
-if(!empty($todoList)){
-	foreach($todoList as $doer=>$todos){
-		//	echo "\$store";
-		//	krumo($store);
-
-		$doerTotal = 0;
-		$listHead="";
-		$listHead.="<span class=\"heading\"><strong>".$doer;
-		$listBody="<ul>";
-		foreach($todos as $todo){
-			$listBody.="<li class=\"litem\"><strong>{$todo->item}</strong> &nbsp;({$todo->duration})";
-			$listBody.="&nbsp;<a class=\"deleteme\" href=\"javascript:confirmDelete('delete.php?TodoList=".urlencode($todo->id)."')\">delete</a>";
-			$listBody.="&nbsp;<a class=\"editme\" href=\"index.php?TodoList=".urlencode($todo->id)."\">edit</a>";
-			$doerTotal+=isset($todo->duration) ? $todo->duration: 0;
-			$listBody.=(strlen($todo->description)>0)?"<br/><span class=\"description\">".$todo->description."</span>":"";
-			$listBody.="</li>";
-		}
-		$listHead.="</strong><span class=\"workAggregate\">&nbsp;&nbsp;(".$doerTotal.")</span></span>";
-		$listBody.="</ul>";
-		$list.=$listHead.$listBody;
-	}
-}
+$list.= ListView::RenderTodoList($todoList);
 $list.="</div>";
 
 $content.=$list."</div>";
--- lib/ListView.php
+++ lib/ListView.php
@@ -31,10 +31,37 @@ class ListView {
                 }
                 $listHead.="</strong><span class=\"aggregate\">&nbsp;&nbsp;(\$" . $storeTotal . ")</span></span>";
                 $listBody.="</ul>";
-                $shoppingList = $listHead . $listBody;
+                $shoppingList.= $listHead . $listBody;
             }
         }
         return $shoppingList;
+    }
+
+    public static function RenderTodoList($list = null) {
+        $todoList = null;
+        if (!empty($list)) {
+            foreach ($list as $doer => $todos) {
+                //	echo "\$store";
+                //	krumo($store);
+
+                $doerTotal = 0;
+                $listHead = "";
+                $listHead.="<span class=\"heading\"><strong>" . $doer;
+                $listBody = "<ul>";
+                foreach ($todos as $todo) {
+                    $listBody.="<li class=\"litem\"><strong>{$todo->item}</strong> &nbsp;({$todo->duration})";
+                    $listBody.="&nbsp;<a class=\"deleteme\" href=\"javascript:confirmDelete('delete.php?TodoList=" . urlencode($todo->id) . "')\">delete</a>";
+                    $listBody.="&nbsp;<a class=\"editme\" href=\"index.php?TodoList=" . urlencode($todo->id) . "\">edit</a>";
+                    $doerTotal+=isset($todo->duration) ? $todo->duration : 0;
+                    $listBody.=(strlen($todo->description) > 0) ? "<br/><span class=\"description\">" . $todo->description . "</span>" : "";
+                    $listBody.="</li>";
+                }
+                $listHead.="</strong><span class=\"workAggregate\">&nbsp;&nbsp;(" . $doerTotal . ")</span></span>";
+                $listBody.="</ul>";
+                $todoList.=$listHead . $listBody;
+            }
+        }
+        return $todoList;
     }
 
 }
