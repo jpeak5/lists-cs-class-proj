08f61fca13a5aa0ea56b8a4bf08843c0a836523c
jason peak <jpeak5@lsu.edu>
2012-10-15 21:27:21 -0500

    fixes code formatting; Removes debug print_r

--- form.php
+++ form.php
@@ -4,20 +4,25 @@ require_once(SPYC);
 
 global $logger;
 
-print_r($_POST);
+//print_r($_POST);
 if(isset($_POST['form'])){
-	$logger->log(0,"form.php", "\$post is set proceeding to process request");
+    $logger->log(0,"form.php", 
+        "\$post is set proceeding to process request");
 	
 	
 	if($_POST['form']=="ShoppingList"){
-	$form = new Form("index.php", FORMS_PATH.DS."todoInputs.yaml", "TodoList");
+	    $form = new Form("index.php", 
+            FORMS_PATH.DS."todoInputs.yaml", "TodoList");
 	}elseif($_POST['form']=="TodoList"){
-		$form = new Form("index.php", FORMS_PATH.DS."formInput.yaml", "ShoppingList");
+        $form = new Form("index.php", 
+            FORMS_PATH.DS."formInput.yaml", "ShoppingList");
 	}
 
 	$page=$form->toString();
-	echo $page;
-	$logger->log(0,"form.php", "presenting form defined in {$formInput}");
+
+    echo $page;
+
+    $logger->log(0,"form.php", "presenting form defined in {$formInput}");
 
 }elseif(isset($_POST['ShoppingList']) || isset($_POST['TodoList'])){
 	//then we'll edit the id passed as its value
